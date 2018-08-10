d98e37174dabfb9815547c8ff63b031c4a06f766
Patrick McPartland <patrick.c.mcpartland@gmail.com>
2018-08-10 02:29:37 -0600

    Added vars and basic HTML

---
+++ javascript/game.js
@@ -0,0 +1,6 @@
+var wins = 0
+var losses = 0
+var compGuess = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var ourGuess = []
+var guessLeft = 9
+
