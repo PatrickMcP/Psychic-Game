c15fb4cbde224f7017e95f18fc26e55afc74b234
Patrick McPartland <patrick.c.mcpartland@gmail.com>
2018-08-13 13:54:48 -0600

    Final commit

--- javascript/game.js
+++ javascript/game.js
@@ -1,6 +1,49 @@
 var wins = 0
 var losses = 0
-var compGuess = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-var ourGuess = []
+var compChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var compGuess = []
 var guessLeft = 9
-
+var guessMade = []
+//start up the game
+psychicGame();
+// Define main Game function
+function psychicGame() {
+    //generate a guess for the computer
+    var compGuess = compChoices[Math.floor(Math.random() * compChoices.length)];
+    //check user guesses vs stored computer value
+    document.onkeyup = function (event) {
+        var userChoice = String.fromCharCode(event.keyCode).toLowerCase();
+        // Make Sure the user picked a letter
+        if (event.keyCode < 65 || event.keyCode > 90) {
+            alert("Please pick a letter");
+        //check if correct guess
+        }
+        else if (userChoice == compGuess) {
+            wins++;
+            document.getElementById("wins").innerHTML = "Wins: " + wins;
+            resetGame();
+            alert("You Win!")
+        }
+        //lower count of guess left
+        else if ((userChoice != compGuess) && (guessLeft > 0)) {
+            guessLeft--;
+            guessMade.push(userChoice);
+            document.getElementById("guessmade").innerHTML = "Guesses made: " +guessMade;
+            document.getElementById("guessleft").innerHTML = "Guesses left: " +guessLeft;
+        }
+        //count loss at end of guess/reset game
+        else {
+            losses++
+            document.getElementById("losses").innerHTML = "Losses: " + losses;
+            console.log("You loose!")
+            resetGame();
+            alert("You Loose =(")
+        }
+    }
+}
+//Reset the game at win or when out of guesses
+function resetGame () {
+    guessLeft = 9;
+    guessMade = [];
+    psychicGame ();
+}
